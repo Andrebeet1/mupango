d6f9c3d9465118e821531045d4663d150988e711
andre <andrebeet1@gmail.com>
2025-04-18 01:37:36 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('inscription/', views.inscription_sous_admin, name='inscription'),
-    path('creer_groupe/', views.creer_groupe, name='creer_groupe'),
-    path('<int:groupe_id>/activites/', views.liste_activites, name='liste_activites'),
+    path('admin/', admin.site.urls),
+    path('groupes/', include('groupes.urls')),
 ]
